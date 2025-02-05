49dba359730324011a86bcd81ae48ecd6f8736fb
Ho Thanh Kien <143081364+KuaDtrai@users.noreply.github.com>
2025-02-05 16:36:40 +0700

    Initialized

---
+++ src/main/java/G5_SWP391/ChildGrownTracking/ChildGrownTrackingApplication.java
@@ -0,0 +1,13 @@
+package G5_SWP391.ChildGrownTracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildGrownTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChildGrownTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/G5_SWP391/ChildGrownTracking/ChildGrownTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package G5_SWP391.ChildGrownTracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildGrownTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
